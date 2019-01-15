14564547d612bfd3a7ee77951742d2dba8349cdd
sbaudoin <sylvain.baudoin@gmail.com>
2019-01-15 23:29:54 +0100

    Added new rule quoted-strings

---
+++ src/main/java/com/github/sbaudoin/yamllint/rules/QuotedStrings.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2018, Sylvain Baudoin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.sbaudoin.yamllint.rules;
+
+import com.github.sbaudoin.yamllint.LintProblem;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.nodes.NodeId;
+import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.resolver.Resolver;
+import org.yaml.snakeyaml.tokens.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Use this rule to forbid any string values that are not quoted. You can also enforce the type of the quote used using
+ * the {@code quote-type} option ({@code single}, {@code double} or {@code any}).
+ *
+ * <p><strong>Note</strong>: Multi-line strings (with {@code |} or {@code >}) will not be checked.</p>
+ *
+ * <p>Examples:</p>
+ * <p>With <code>quoted-strings: {quote-type: any}</code>
+ * the following code snippet would **PASS**:
+ * <pre>
+ *     foo: "bar"
+ *     bar: 'foo'
+ *     number: 123
+ *     boolean: true
+ * </pre>
+ * the following code snippet would **FAIL**:
+ * <pre>
+ *     foo: bar
+ * </pre>
+ */
+public class QuotedStrings extends TokenRule {
+    public static final String OPTION_QUOTE_TYPE = "quote-type";
+    public static final String QUOTE_STYLE_SINGLE = "single";
+    public static final String QUOTE_STYLE_DOUBLE = "double";
+    public static final String QUOTE_STYLE_ANY = "any";
+
+
+    public QuotedStrings() {
+        options.put(OPTION_QUOTE_TYPE, Arrays.asList(QUOTE_STYLE_SINGLE, QUOTE_STYLE_DOUBLE, QUOTE_STYLE_ANY));
+    }
+
+    @Override
+    public List<LintProblem> check(Map conf, Token token, Token prev, Token next, Token nextnext, Map<String, Object> context) {
+        String quoteType = (String) conf.getOrDefault(OPTION_QUOTE_TYPE, QUOTE_STYLE_ANY);
+
+        List<LintProblem> problems = new ArrayList<>();
+
+        if (token instanceof ScalarToken && (prev instanceof ValueToken || prev instanceof TagToken)) {
+            // Ignore explicit types, e.g. !!str testtest or !!int 42
+            if (prev instanceof TagToken && "!!".equals(((TagToken) prev).getValue().getHandle())) {
+                return problems;
+            }
+
+            // Ignore numbers, booleans, etc.
+            Resolver resolver = new Resolver();
+            if (resolver.resolve(NodeId.scalar, ((ScalarToken) token).getValue(), true) != Tag.STR) {
+                return problems;
+            }
+
+            // Ignore multi-line strings
+            if (!((ScalarToken) token).getPlain() && (
+                    ((ScalarToken) token).getStyle() == DumperOptions.ScalarStyle.LITERAL ||
+                            ((ScalarToken) token).getStyle() == DumperOptions.ScalarStyle.FOLDED)) {
+                return problems;
+            }
+
+            if ((quoteType.equals(QUOTE_STYLE_SINGLE) && ((ScalarToken) token).getStyle() != DumperOptions.ScalarStyle.SINGLE_QUOTED) ||
+                    (quoteType.equals(QUOTE_STYLE_DOUBLE) && ((ScalarToken) token).getStyle() != DumperOptions.ScalarStyle.DOUBLE_QUOTED) ||
+                    (quoteType.equals(QUOTE_STYLE_ANY) && ((ScalarToken) token).getStyle() == DumperOptions.ScalarStyle.PLAIN)) {
+                problems.add(new LintProblem(token.getStartMark().getLine() + 1,
+                        token.getStartMark().getColumn() + 1,
+                        "string value is not quoted with " + quoteType + " quotes"));
+            }
+        }
+
+        return problems;
+    }
+}
---
+++ src/test/java/com/github/sbaudoin/yamllint/rules/QuotedStringsTest.java
@@ -0,0 +1,129 @@
+/**
+ * Copyright (c) 2018, Sylvain Baudoin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.sbaudoin.yamllint.rules;
+
+import com.github.sbaudoin.yamllint.YamlLintConfig;
+import com.github.sbaudoin.yamllint.YamlLintConfigException;
+
+import java.io.IOException;
+
+public class QuotedStringsTest extends RuleTester {
+    public void testDisabled() throws IOException, YamlLintConfigException {
+        YamlLintConfig conf = getConfig("quoted-strings: disable");
+        check("---\n" +
+                "foo: bar\n", conf);
+        check("---\n" +
+                "foo: \"bar\"\n", conf);
+        check("---\n" +
+                "foo: 'bar'\n", conf);
+        check("---\n" +
+                "bar: 123\n", conf);
+    }
+
+    public void testQuoteTypeAny() throws IOException, YamlLintConfigException {
+        YamlLintConfig conf = getConfig("quoted-strings: {quote-type: any}");
+        check("---\n" +
+                "boolean1: true\n" +
+                "number1: 123\n" +
+                "string1: foo\n" +                          // fails
+                "string2: \"foo\"\n" +
+                "string3: 'bar'\n" +
+                "string4: !!str genericstring\n" +
+                "string5: !!str 456\n" +
+                "string6: !!str \"quotedgenericstring\"\n" +
+                "binary: !!binary Ymluc3RyaW5n\n" +
+                "integer: !!int \"12\"\n" +
+                "boolean2: !!bool boolstring\n" +
+                "boolean3: !!bool \"quotedboolstring\"\n", conf, getLintProblem(4, 10));
+        check("---\n" +
+                "multiline string 1: |\n" +
+                "  line 1\n" +
+                "  line 2\n" +
+                "multiline string 2: >\n" +
+                "  word 1\n" +
+                "  word 2\n" +
+                "multiline string 3:\n" +
+                "  word 1\n" +
+                "  word 2\n" +
+                "multiline string 4:\n" +
+                "  \"word 1\\\n" +
+                "   word 2\"\n", conf, getLintProblem(9, 3));
+    }
+
+    public void testQuoteTypeSingle() throws IOException, YamlLintConfigException {
+        YamlLintConfig conf = getConfig("quoted-strings: {quote-type: single}");
+        check("---\n" +
+                "boolean1: true\n" +
+                "number1: 123\n" +
+                "string1: foo\n" +                          // fails
+                "string2: \"foo\"\n" +                        // fails
+                "string3: 'bar'\n" +
+                "string4: !!str genericstring\n" +
+                "string5: !!str 456\n" +
+                "string6: !!str \"quotedgenericstring\"\n" +
+                "binary: !!binary Ymluc3RyaW5n\n" +
+                "integer: !!int \"12\"\n" +
+                "boolean2: !!bool boolstring\n" +
+                "boolean3: !!bool \"quotedboolstring\"\n", conf,
+                getLintProblem(4, 10), getLintProblem(5, 10));
+        check("---\n" +
+                "multiline string 1: |\n" +
+                "  line 1\n" +
+                "  line 2\n" +
+                "multiline string 2: >\n" +
+                "  word 1\n" +
+                "  word 2\n" +
+                "multiline string 3:\n" +
+                "  word 1\n" +
+                "  word 2\n" +
+                "multiline string 4:\n" +
+                "  \"word 1\\\n" +
+                "   word 2\"\n", conf,
+                getLintProblem(9, 3), getLintProblem(12, 3));
+    }
+
+    public void testQuoteTypeDouble() throws IOException, YamlLintConfigException {
+        YamlLintConfig conf = getConfig("quoted-strings: {quote-type: double}");
+        check("---\n" +
+                "boolean1: true\n" +
+                "number1: 123\n" +
+                "string1: foo\n" +                          // fails
+                "string2: \"foo\"\n" +
+                "string3: 'bar'\n" +                      // fails
+                "string4: !!str genericstring\n" +
+                "string5: !!str 456\n" +
+                "string6: !!str \"quotedgenericstring\"\n" +
+                "binary: !!binary Ymluc3RyaW5n\n" +
+                "integer: !!int \"12\"\n" +
+                "boolean2: !!bool boolstring\n" +
+                "boolean3: !!bool \"quotedboolstring\"\n", conf,
+                getLintProblem(4, 10), getLintProblem(6, 10));
+        check("---\n" +
+                "multiline string 1: |\n" +
+                "  line 1\n" +
+                "  line 2\n" +
+                "multiline string 2: >\n" +
+                "  word 1\n" +
+                "  word 2\n" +
+                "multiline string 3:\n" +
+                "  word 1\n" +
+                "  word 2\n" +
+                "multiline string 4:\n" +
+                "  \"word 1\\\n" +
+                "   word 2\"\n", conf,
+                getLintProblem(9, 3));
+    }
+}
