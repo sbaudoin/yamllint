236e9d5ec3bfb4ee5dcfd225928304adc33614c1
sbaudoin <sylvain.baudoin@gmail.com>
2023-11-05 23:40:23 +0100

    Add new option to detect unused anchors

--- src/main/java/com/github/sbaudoin/yamllint/rules/Anchors.java
+++ src/main/java/com/github/sbaudoin/yamllint/rules/Anchors.java
@@ -19,6 +19,8 @@ import com.github.sbaudoin.yamllint.LintProblem;
 import org.yaml.snakeyaml.tokens.*;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Use this rule to report duplicated anchors and aliases referencing undeclared anchors.
@@ -29,6 +31,8 @@ import java.util.*;
  *         later in the document).</li>
  *     <li>Set {@code forbid-duplicated-anchors} to {@code true} to avoid duplications of a same
  *         anchor.</li>
+ *     <li>Set {@code forbid-unused-anchors} to {@code true} to avoid anchors being declared but
+ *         not used anywhere in the YAML document via alias.</li>
  * </ul>
  *
  * <p>Default values (when enabled):</p>
@@ -37,6 +41,7 @@ import java.util.*;
  *       anchors:
  *         forbid-undeclared-aliases: true
  *         forbid-duplicated-anchors: false
+ *         forbid-unused-anchors: false
  * </pre>
  *
  * <p>Examples:</p>
@@ -76,6 +81,24 @@ import java.util.*;
  *     - &anchor Foo Bar
  *     - &anchor [item 1, item 2]
  * </pre>
+ *
+ * <p>With <code>anchors: {forbid-unused-anchors: true}</code>
+ * the following code snippet would **PASS**:
+ * <pre>
+ *     ---
+ *     - &anchor
+ *       foo: bar
+ *     - *anchor
+ * </pre>
+ * the following code snippet would **FAIL**:
+ * <pre>
+ *     ---
+ *     - &anchor
+ *       foo: bar
+ *     - items:
+ *       - item1
+ *       - item2
+ * </pre>
  */
 public class Anchors extends TokenRule {
     private static final String ANCHORS_KEY = "anchors";
@@ -83,41 +106,93 @@ public class Anchors extends TokenRule {
 
     public static final String OPTION_FORBID_UNDECLARED_ALIASES = "forbid-undeclared-aliases";
     public static final String OPTION_FORBID_DUPLICATED_ANCHORS = "forbid-duplicated-anchors";
+    public static final String OPTION_FORBID_UNUSED_ANCHORS     = "forbid-unused-anchors";
 
 
     public Anchors() {
         registerOption(OPTION_FORBID_UNDECLARED_ALIASES, true);
         registerOption(OPTION_FORBID_DUPLICATED_ANCHORS, false);
+        registerOption(OPTION_FORBID_UNUSED_ANCHORS, false);
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public List<LintProblem> check(Map<Object, Object> conf, Token token, Token prev, Token next, Token nextnext, Map<String, Object> context) {
         List<LintProblem> problems = new ArrayList<>();
 
-        if (Boolean.TRUE.equals(conf.get(OPTION_FORBID_UNDECLARED_ALIASES)) || Boolean.TRUE.equals(conf.get(OPTION_FORBID_DUPLICATED_ANCHORS))) {
-            if (token instanceof StreamStartToken || token instanceof DocumentStartToken) {
-                context.put(ANCHORS_KEY, new HashSet<String>());
+        if (Boolean.TRUE.equals(conf.get(OPTION_FORBID_UNDECLARED_ALIASES)) ||
+                Boolean.TRUE.equals(conf.get(OPTION_FORBID_DUPLICATED_ANCHORS)) ||
+                Boolean.TRUE.equals(conf.get(OPTION_FORBID_UNUSED_ANCHORS))) {
+            if (token instanceof StreamStartToken ||
+                    token instanceof DocumentStartToken ||
+                    token instanceof DocumentEndToken) {
+                context.put(ANCHORS_KEY, new HashMap<String, Object>());
             }
         }
 
         if (Boolean.TRUE.equals(conf.get(OPTION_FORBID_UNDECLARED_ALIASES)) && token instanceof AliasToken &&
-                !((Set<?>)context.get(ANCHORS_KEY)).contains(((AliasToken)token).getValue())) {
+                !getContextMap(context).containsKey(((AliasToken)token).getValue())) {
             problems.add(new LintProblem(token.getStartMark().getLine() + 1, token.getStartMark().getColumn() + 1,
                     String.format("found undeclared alias \"%s\"", ((AliasToken)token).getValue())));
         }
 
         if (Boolean.TRUE.equals(conf.get(OPTION_FORBID_DUPLICATED_ANCHORS)) && token instanceof AnchorToken &&
-                ((Set<?>)context.get(ANCHORS_KEY)).contains(((AnchorToken)token).getValue())) {
+                getContextMap(context).containsKey(((AnchorToken)token).getValue())) {
             problems.add(new LintProblem(token.getStartMark().getLine() + 1, token.getStartMark().getColumn() + 1,
                     String.format("found duplicated anchor \"%s\"", ((AnchorToken) token).getValue())));
         }
 
-        if (Boolean.TRUE.equals(conf.get(OPTION_FORBID_UNDECLARED_ALIASES)) || Boolean.TRUE.equals(conf.get(OPTION_FORBID_DUPLICATED_ANCHORS))) {
+        if (Boolean.TRUE.equals(conf.get(OPTION_FORBID_UNUSED_ANCHORS))) {
+            // Unused anchors can only be detected at the end of Document.
+            // End of document can be either
+            //   - end of stream
+            //   - end of document sign '...'
+            //   - start of a new document sign '---'
+            // If next token indicates end of document,
+            // check if the anchors have been used or not.
+            // If they haven't been used, report problem on those anchors.
+            if (next instanceof StreamEndToken ||
+                    next instanceof DocumentStartToken ||
+                    next instanceof DocumentEndToken) {
+                for (String k : getContextMap(context).keySet()) {
+                    Map<String, Object> info = (Map<String, Object>) getContextMap(context).get(k);
+                    if (Boolean.FALSE.equals(info.get("used"))) {
+                        problems.add(new LintProblem((Integer)info.get("line") + 1,
+                                (Integer)info.get("column") + 1,
+                            String.format("found unused anchor \"%s\"", k)));
+                    }
+                }
+            } else if (token instanceof AliasToken) {
+                ((HashMap<String, Object>) (getContextMap(context).getOrDefault(
+                        ((AliasToken)token).getValue(), new HashMap<String, Object>()))).put("used", true);
+            }
+        }
+
+        if (Boolean.TRUE.equals(conf.get(OPTION_FORBID_UNDECLARED_ALIASES)) ||
+                Boolean.TRUE.equals(conf.get(OPTION_FORBID_DUPLICATED_ANCHORS)) ||
+                Boolean.TRUE.equals(conf.get(OPTION_FORBID_UNUSED_ANCHORS))) {
             if (token instanceof AnchorToken) {
-                ((Set<String>)context.get(ANCHORS_KEY)).add(((AnchorToken)token).getValue());
+                getContextMap(context).put(((AnchorToken) token).getValue(),
+                        Stream.of(
+                                new AbstractMap.SimpleEntry<>("line", token.getStartMark().getLine()),
+                                new AbstractMap.SimpleEntry<>("column", token.getStartMark().getColumn()),
+                                new AbstractMap.SimpleEntry<>("used", false))
+                                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
             }
         }
 
         return problems;
+    }
+
+
+    /**
+     * Method used to simplify the syntax of the {@code check} method
+     *
+     * @param context a context map used to propagate info between rules
+     * @return a map used to store information about the tokens being checked
+     */
+    @SuppressWarnings("unchecked")
+    private Map<String, Object> getContextMap(Map<String, Object> context) {
+        return (Map<String, Object>) context.get(ANCHORS_KEY);
     }
 }
--- src/test/java/com/github/sbaudoin/yamllint/rules/AnchorsTest.java
+++ src/test/java/com/github/sbaudoin/yamllint/rules/AnchorsTest.java
@@ -81,7 +81,8 @@ public class AnchorsTest extends RuleTester {
     public void testForbidUndeclaredAliases() throws YamlLintConfigException {
         YamlLintConfig conf = getConfig("anchors:",
                 "  forbid-undeclared-aliases: true",
-                "  forbid-duplicated-anchors: false");
+                "  forbid-duplicated-anchors: false",
+                "  forbid-unused-anchors: false");
         check("---\n" +
                 "- &b true\n" +
                 "- &i 42\n" +
@@ -123,6 +124,7 @@ public class AnchorsTest extends RuleTester {
                 "- *f_m\n" +
                 "- *f_s\n" +                     // declared after
                 "- &f_s [1, 2]\n" +
+                "...\n" +
                 "---\n" +
                 "block mapping: &b_m\n" +
                 "  key: value\n" +
@@ -142,14 +144,15 @@ public class AnchorsTest extends RuleTester {
                 getLintProblem(11, 3),
                 getLintProblem(12, 3),
                 getLintProblem (13, 3),
-                getLintProblem(24, 7),
-                getLintProblem(27, 37));
+                getLintProblem(25, 7),
+                getLintProblem(28, 37));
     }
 
     public void testForbidDuplicatedAnchors() throws YamlLintConfigException {
         YamlLintConfig conf = getConfig("anchors:",
                "  forbid-undeclared-aliases: false",
-                "  forbid-duplicated-anchors: true");
+                "  forbid-duplicated-anchors: true",
+                "  forbid-unused-anchors: false");
         check("---\n" +
                 "- &b true\n" +
                 "- &i 42\n" +
@@ -191,6 +194,7 @@ public class AnchorsTest extends RuleTester {
                 "- *f_m\n" +
                 "- *f_s\n" +                     // declared after
                 "- &f_s [1, 2]\n" +
+                "...\n" +
                 "---\n" +
                 "block mapping: &b_m\n" +
                 "  key: value\n" +
@@ -207,7 +211,76 @@ public class AnchorsTest extends RuleTester {
                 "...\n", conf,
                 getLintProblem(5, 3),
                 getLintProblem(6, 3),
-                getLintProblem(21, 18),
-                getLintProblem(27, 20));
+                getLintProblem(22, 18),
+                getLintProblem(28, 20));
+    }
+
+    public void testForbidUnusedAnchors() throws YamlLintConfigException {
+        YamlLintConfig conf = getConfig("anchors:",
+                "  forbid-undeclared-aliases: false",
+                "  forbid-duplicated-anchors: false",
+                "  forbid-unused-anchors: true");
+
+        check("---\n" +
+                "- &b true\n" +
+                "- &i 42\n" +
+                "- &s hello\n" +
+                "- &f_m {k: v}\n" +
+                "- &f_s [1, 2]\n" +
+                "- *b\n" +
+                "- *i\n" +
+                "- *s\n" +
+                "- *f_m\n" +
+                "- *f_s\n" +
+                "---\n" +                        // redeclare anchors in a new document
+                "- &b true\n" +
+                "- &i 42\n" +
+                "- &s hello\n" +
+                "- *b\n" +
+                "- *i\n" +
+                "- *s\n" +
+                "---\n" +
+                "block mapping: &b_m\n" +
+                "  key: value\n" +
+                "extended:\n" +
+                "  <<: *b_m\n" +
+                "  foo: bar\n" +
+                "---\n" +
+                "{a: 1, &x b: 2, c: &y 3, *x : 4, e: *y}\n" +
+                "...\n", conf);
+        check("---\n" +
+                "- &i 42\n" +
+                "---\n" +
+                "- &b true\n" +
+                "- &b true\n" +
+                "- &b true\n" +
+                "- &s hello\n" +
+                "- *b\n" +
+                "- *i\n" +                       // declared in a previous document
+                "- *f_m\n" +                     // never declared
+                "- *f_m\n" +
+                "- *f_m\n" +
+                "- *f_s\n" +                     // declared after
+                "- &f_s [1, 2]\n" +
+                "...\n" +
+                "---\n" +
+                "block mapping: &b_m\n" +
+                "  key: value\n" +
+                "---\n" +
+                "block mapping 1: &b_m_bis\n" +
+                "  key: value\n" +
+                "block mapping 2: &b_m_bis\n" +
+                "  key: value\n" +
+                "extended:\n" +
+                "  <<: *b_m\n" +
+                "  foo: bar\n" +
+                "---\n" +
+                "{a: 1, &x b: 2, c: &x 3, *x : 4, e: *y}\n" +
+                "...\n", conf,
+                getLintProblem(2, 3),
+                getLintProblem(7, 3),
+                getLintProblem(14, 3),
+                getLintProblem(17, 16),
+                getLintProblem(22, 18));
     }
 }
